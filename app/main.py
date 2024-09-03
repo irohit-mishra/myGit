e4e9ba97fb26a78b9df064a123cae816f42cb1af
Rohit Mishra <iamrohit330@gmail.com>
2024-09-03 09:32:47 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -23,6 +23,7 @@ def main():
                 header, content = raw.split(b"\0", maxsplit=1)
                 print(content.decode("utf-8"), end="")
 
+
     elif command == "hash-object":
         if sys.argv[2] == "-w":
             file_path = sys.argv[3]
@@ -50,6 +51,18 @@ def main():
 
             # Print the SHA-1 hash
             print(sha1)
+
+    elif command == "ls-tree":
+        param, hash = sys.argv[2], sys.argv[3]
+        if param == "--name-only":
+            with open(f".git/objects/{hash[:2]}/{hash[2:]}", "rb") as f:
+                data = zlib.decompress(f.read())
+                _, binary_data = data.split(b"\x00", maxsplit=1)
+            while binary_data:
+                mode, binary_data = binary_data.split(b"\x00", maxsplit=1)
+                _, name = mode.split()
+                binary_data = binary_data[20:]
+                print(name.decode(utf-8))
 
     else:
         raise RuntimeError(f"Unknown command #{command}")
