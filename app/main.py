f47df046968af8bd03d6611eb8ae58d4677237d5
Rohit Mishra <iamrohit330@gmail.com>
2024-09-03 10:28:32 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -3,7 +3,42 @@ import os
 import zlib
 import hashlib
 
-
+def create_blob_entry(path, write=True):
+    with open(path,"rb") as f:
+        data = f.read()
+    header = f"blob {len(data)}\0".encode("utf-8")
+    store = header + data
+    sha = hashlib.sha1(store).hexdigest()
+    if write:
+        os.mkdirs(f".git/objects/{sha[:2]}", exist_ok=True)
+        with open(f".git/objects/{sha[:2]}/{sha[2:]}", "wb") as f:
+             f.write(zlib.compress(store))
+        return sha
+    
+def write_tree(path: str):
+    if os.path.isfile(path):
+        return create_blob_entry(path)
+    contents = sorted(
+        os.listdir(path),
+        key=lambda x:x if os.path.isfile(os.path.join(path,x)) else f"{x}/",
+    )
+    s = b""
+    for item in contents:
+        if item == ".git":
+            continue
+        full = os.path.join(path, item)
+        if os.path.isfile(full):
+            s += f"100644{item}\0".encode()
+        else:
+            s += f"40000{item}\0".encode()
+        sha1 = int.to_bytes(int(write_tree(full), base=16), length=20, byteorder="big")
+        s += sha1
+    s= f"tree{len(s)}\0".encode() + s
+    sha1 = hashlib.sha1(s).hexdigest()
+    os.makedirs(f".git/objects/{sha1[:2]}", exist_ok=True)
+    with open(f".git/objects/{sha1[:2]}/{sha1[2:]}", "wb") as f:
+        f.write(zlib.compress(s))
+    return sha1
 def main():
     command = sys.argv[1]
 
@@ -63,6 +98,9 @@ def main():
                 _, name = mode.split()
                 binary_data = binary_data[20:]
                 print(name.decode("utf-8"))
+
+    elif command =="write-tree":
+        print(write_tree("./"))
 
     else:
         raise RuntimeError(f"Unknown command #{command}")
